42d9eaa0b9cc402e1405795a6709d53d8745eba4
Rafael Santos <rafaelfilipedoss@gmail.com>
2018-10-25 09:09:55 -0300

    passando dados para o popover

--- src/app/editar-modal/editar-modal.component.ts
+++ src/app/editar-modal/editar-modal.component.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
 
 @Component({
   selector: 'app-editar-modal',
   templateUrl: './editar-modal.component.html',
   styleUrls: ['./editar-modal.component.scss']
 })
-export class EditarModalComponent {
+export class EditarModalComponent implements OnInit{
+  modal: any;
 
   constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<EditarModalComponent>
-    ) {}
+  ) {
+    
+  }
   
+  ngOnInit() {
+    this.modal = this.data.modal;
+  }
     
   onNoClick(): void {
     console.log(this.dialogRef);
